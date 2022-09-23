d8cf265a007af3bbb5ca42585e50c162f41c8b2e
Pawel Pasek <pasekpasek@gmail.com>
2022-09-23 06:54:44 +0200

    w01 - wip

--- week-1-ts-ecosystem/w01e01-typescript-basics/src/main.ts
+++ week-1-ts-ecosystem/w01e01-typescript-basics/src/main.ts
@@ -6,7 +6,21 @@
  *
  * */
 
-const user: any = {
+type UserAdress = {
+  street: string,
+  no: number
+}
+
+type User = {
+  name: string,
+  age: number,
+  email: string,
+  address?: UserAdress
+}
+
+type AgeChecker = (User)=> boolean;
+
+const user: User = {
   name: 'Andy',
   age: 30,
   email: 'andy@mail-me-tommorow.com',
@@ -16,12 +30,12 @@ const user: any = {
   },
 }
 
-function hasAddress(user: any): any {
+function hasAddress(user: User): boolean {
   return Boolean(user.address)
 }
 
-function hasGivenAge(requiredAge: any): any {
-  return (user: any): any => user.age >= requiredAge
+function hasGivenAge(requiredAge: number): AgeChecker {
+  return (user: User): boolean => user.age >= requiredAge
 }
 
 const isAdult = hasGivenAge(18)
